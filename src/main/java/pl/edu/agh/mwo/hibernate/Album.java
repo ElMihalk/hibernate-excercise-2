25fe054c7ba577b75094df165533ef283d6cc9fb
mikostrz <mikostrz@d17-430-pc-12.ipa.ii.agh.edu.pl>
2025-02-09 15:55:40 +0100

    Database initiated.

--- src/main/java/pl/edu/agh/mwo/hibernate/Album.java
+++ src/main/java/pl/edu/agh/mwo/hibernate/Album.java
@@ -58,4 +58,10 @@ public class Album {
     public String toString() {
         return "Album{" + "id=" + id + ", name=" + name + ", description=" + description + '}';
     }
+
+//    CREATE TABLE albums (id INTEGER PRIMARY KEY, name TEXT, description TEXT);
+//    INSERT INTO albums (id, name, description) VALUES (1, 'Album 1', 'Zwierzęta');
+//    INSERT INTO albums (id, name, description) VALUES (2, 'Album 2', 'Rośliny');
+//    INSERT INTO albums (id, name, description) VALUES (3, 'Album 3', 'Zabytki');
+//    INSERT INTO albums (id, name, description) VALUES (4, 'Album 4', 'Widoki');
 }
